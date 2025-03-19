e8474ba52f84e329c13e9a926fef1b79f8bc8e9c
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2025-03-19 14:37:25 +0700

    created header

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './index.css'
 
 const App = () => {
   return (
-    <h1>hello react!</h1>
+    <BrowserRouter>
+      <header>
+        <h1>
+          <Link>#vanslife</Link>
+        </h1>
+        <nav className='nav'>
+          <Link className='about'>About</Link>
+          <Link>vans</Link>
+        </nav>
+      </header>
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
